02b9cc315c1e96f7e91acac102e23aeb1ea46605
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-06-25 09:58:31 +0300

    save start files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,35 @@
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Container, Header, Section, Text } from 'components';
+import { Gallery, Todos } from 'tabs';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Section>
+        <Container>
+          <Tabs>
+            <TabList>
+              <Tab>
+                <Text>Gallery</Text>
+              </Tab>
+              <Tab>
+                <Text>Todos</Text>
+              </Tab>
+            </TabList>
+
+            <TabPanel>
+              <Gallery />
+            </TabPanel>
+
+            <TabPanel>
+              <Todos />
+            </TabPanel>
+          </Tabs>
+        </Container>
+      </Section>
+    </>
+  );
+};
