3eb7f332241c38306e63b36baa46b1a50656002e
TetiPalii <TetianaPalii9491@gmail.com>
2023-07-23 15:18:42 +0200

    task2

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,51 +1,19 @@
-import { useState } from 'react';
 import { Grid, GridItem, SearchForm, EditForm, Todo } from 'components';
 import { useSelector } from 'react-redux';
 import { selecSelectTodo, selectTodos } from 'redux/selectors';
 
 export const Todos = () => {
-  const [, setTodos] = useState([]);
   const todos = useSelector(selectTodos);
   const selectedTodo = useSelector(selecSelectTodo);
-  const [, setSelectedTodo] = useState(null);
-
-  const selectTodo = id => {
-    const currentTodo = todos.find(todo => todo.id === id);
-    setSelectedTodo(currentTodo);
-  };
-
-  const editTodos = editedTodo => {
-    setTodos(prevTodos =>
-      prevTodos.map(todo => (todo.id === editedTodo.id ? editedTodo : todo))
-    );
-    setSelectedTodo(null);
-  };
-
-  const closeForm = () => {
-    setSelectedTodo(null);
-  };
 
   return (
     <>
-      {selectedTodo ? (
-        <EditForm
-          selectedTodo={selectedTodo}
-          editTodos={editTodos}
-          closeForm={closeForm}
-        />
-      ) : (
-        <SearchForm />
-      )}
+      {selectedTodo ? <EditForm /> : <SearchForm />}
 
       <Grid>
         {todos.map((todo, idx) => (
           <GridItem key={todo.id}>
-            <Todo
-              id={todo.id}
-              textTodo={todo.text}
-              todoNumber={idx + 1}
-              selectTodo={selectTodo}
-            />
+            <Todo id={todo.id} textTodo={todo.text} todoNumber={idx + 1} />
           </GridItem>
         ))}
       </Grid>
