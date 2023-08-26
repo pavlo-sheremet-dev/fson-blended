c8f2d3717ce0f69dc4c9e790d6bde929f44716ff
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-08-26 21:35:58 +0300

    feat: add SerachForm, complete addTodo logic

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,36 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = ev => {
+    this.setState({ search: ev.target.value });
+  };
+
+  handleSubmit = ev => {
+    ev.preventDefault();
+    this.props.onSubmit({ ...this.state });
+    // resetForm by clear state property
+    this.setState({ search: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          value={this.state.search}
+          onChange={this.handleChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,28 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  addTodo = data => {
+    const todo = {
+      todoText: data.search,
+      id: nanoid(),
+    };
+    this.setState(prevState => {
+      return { todos: [...prevState.todos, todo] };
+    });
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    console.log(this.state.todos);
+    return (
+      <>
+        <SearchForm onSubmit={this.addTodo} />
+
+        {JSON.stringify(this.state.todos)}
+      </>
+    );
   }
 }
