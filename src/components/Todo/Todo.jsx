6d6ba545c20a5310ac23383bb0da8e7a1437facf
Dan Chernov <120645560+ChernovDan42@users.noreply.github.com>
2023-10-08 11:05:10 +0200

    Todos(delete,localStorage)

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,16 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ text, index, handleDelete, id }) => {
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{index}
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton type="button" onClick={() => handleDelete(id)}>
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -6,8 +6,32 @@ export class Todos extends Component {
   state = {
     todos: [],
   };
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    }
+  }
+
+  componentDidMount() {
+    const todos = localStorage.getItem('todos');
+    const parsedTodos = JSON.parse(todos);
+    if (!parsedTodos) return;
+
+    this.setState({ todos: parsedTodos });
+  }
+
+  handleDeleteToDo = id => {
+    this.setState(prev => ({
+      todos: prev.todos.filter(todo => todo.id !== id),
+    }));
+  };
+
   addTodo = newTodo => {
-    const todo = { id: nanoid(), text: newTodo.search };
+    const todo = {
+      id: nanoid(),
+      text: newTodo.search,
+    };
     this.setState(prevState => {
       return {
         todos: [...prevState.todos, todo],
@@ -15,11 +39,25 @@ export class Todos extends Component {
     });
     // console.log('addTodo', newTodo);
   };
+
   render() {
-    console.log(this.state.todos);
+    // console.log(this.state.todos);
     return (
       <div>
         <SearchForm addTodo={this.addTodo} />
+        <Grid>
+          {this.state.todos.map((todo, index) => (
+            <GridItem key={todo.id}>
+              {' '}
+              <Todo
+                text={todo.text}
+                index={index + 1}
+                handleDelete={this.handleDeleteToDo}
+                id={todo.id}
+              />
+            </GridItem>
+          ))}
+        </Grid>
       </div>
     );
   }
