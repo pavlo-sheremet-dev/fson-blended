014ba1f26bcccaf0fc47cb06275e306d4f062501
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2023-10-22 11:09:42 +0200

    add

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, Todo } from 'components';
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectTodos } from 'redux/todos/selectors';
+import { addTodo, deleteTodo } from 'redux/todos/slice';
 
 export const Todos = () => {
   const [, setTodos] = useState(
@@ -11,25 +12,28 @@ export const Todos = () => {
   );
 
   const todos = useSelector(selectTodos);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
   const handleDeleteToDo = id => {
-    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+    dispatch(deleteTodo(id));
+    // setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
   };
 
-  const addTodo = newTodo => {
+  const addTodo2 = newTodo => {
     const todo = {
       id: nanoid(),
       text: newTodo.search,
     };
-    setTodos(prevState => [...prevState, todo]);
+    dispatch(addTodo(todo));
+    // setTodos(prevState => [...prevState, todo]);
   };
   return (
     <div>
-      <SearchForm addTodo={addTodo} />
+      <SearchForm addTodo={addTodo2} />
       <Grid>
         {todos.map((todo, index) => (
           <GridItem key={todo.id}>
